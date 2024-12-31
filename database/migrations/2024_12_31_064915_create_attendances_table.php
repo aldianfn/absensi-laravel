0c1629616bdc6c52b57adf95a50a83536ab65db2
aldianfn <aldian.1998@gmail.com>
2024-12-31 13:55:31 +0700

    Inisialisasi migrasi attendance

---
+++ database/migrations/2024_12_31_064915_create_attendances_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->time('check_in')->nullable();
+            $table->time('check_out')->nullable();
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->string('location')->nullable();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('attendances', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
+
+        Schema::dropIfExists('attendances');
+    }
+};
