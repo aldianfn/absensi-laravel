d29530983c4f5e44a0620fe1ca7e0d2935c134ee
aldianfn <aldian.1998@gmail.com>
2025-01-04 14:12:27 +0700

    Implementasi desain untuk halaman registrasi

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,30 +6,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
-    <h1>Register</h1>
-    <form action="{{ route('register.process') }}" method="POST">
-        @csrf
-        <div class="">
-            <label for="name">Nama</label>
-            <input type="text" name="name" required>
+    <div class="flex flex-col items-center justify-center min-h-screen w-full">
+        <div class="bg-yellow-200 px-7 py-5 rounded-md w-1/3">
+            <h1 class="text-4xl font-bold text-center mb-5">Register</h1>
+            <form action="{{ route('register.process') }}" method="POST">
+                @csrf
+                <div class="flex flex-col mb-3">
+                    <label class="mb-1" for="name">Name</label>
+                    <input class="px-2 py-1 rounded-sm" type="text" name="name" required>
+                </div>
+                <div class="flex flex-col mb-3">
+                    <label class="mb-1" for="email">Email</label>
+                    <input class="px-2 py-1 rounded-sm" type="email" name="email" required>
+                </div>
+                <div class="flex flex-col mb-3">
+                    <label class="mb-1" for="password">Password</label>
+                    <input class="px-2 py-1 rounded-sm" type="password" name="password" required>
+                </div>
+                <div class="flex flex-col mb-3">
+                    <label class="mb-1" for="password_confirmation">Password Confirmation</label>
+                    <input class="px-2 py-1 rounded-sm" type="password" name="password_confirmation" required>
+                </div>
+                <button type="submit"
+                    class="bg-blue-500 hover:bg-blue-600 w-full py-2 rounded-md mt-5 mb-3">Register</button>
+            </form>
         </div>
-        <div class="">
-            <label for="email">Email</label>
-            <input type="email" name="email" required>
-        </div>
-        <div class="">
-            <label for="password">Password</label>
-            <input type="password" name="password" required>
-        </div>
-        <div class="">
-            <label for="password_confirmation">Konfirmasi Password</label>
-            <input type="password" name="password_confirmation" required>
-        </div>
-        <button type="submit">Register</button>
-    </form>
+    </div>
 </body>
 
 </html>
